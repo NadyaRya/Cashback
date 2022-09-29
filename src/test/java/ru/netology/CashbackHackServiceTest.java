709c797ae14e85f7b836ea21f298e8e4c0db28bf
Nadya <nadya_rya@mail.ru>
2022-09-29 12:30:02 +0300

    error repair

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -45,7 +45,7 @@ public class CashbackHackServiceTest {
     @Test
     public void remain4() {
         int amount = 1000;
-        int expected = 1000;
+        int expected = 0;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         assertEquals(actual, expected);
